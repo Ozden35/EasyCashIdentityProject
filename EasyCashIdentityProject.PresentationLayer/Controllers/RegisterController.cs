5847d0bdd388587e1e8cf76ac1c42c24fb679bb2
Ozden35 <115698170+Ozden35@users.noreply.github.com>
2023-06-25 22:34:40 +0300

    değişiklikler yapıldı

--- EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -30,13 +30,23 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
                     Name = appUserRegisterDto.Name,
                     Surname = appUserRegisterDto.Surname,
                     Email = appUserRegisterDto.Email,
+                    City="aaaa",
+                    District="bbbb",
+                    ImageUrl="cccc"
                 };
                 var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
                 if (result.Succeeded)
                 {
                     return RedirectToAction("Index", "ConfirmMail");
                 }
-            }
+                else
+                {
+                    foreach(var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+			}
             return View();
         }
     }
