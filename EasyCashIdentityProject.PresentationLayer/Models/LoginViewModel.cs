61cc794dcaf2c10e7afa495a7566291420809392
Ozden35 <115698170+Ozden35@users.noreply.github.com>
2023-07-18 20:29:32 +0300

    Login İşlemi

---
+++ EasyCashIdentityProject.PresentationLayer/Models/LoginViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Principal;
+
+namespace EasyCashIdentityProject.PresentationLayer.Models
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
